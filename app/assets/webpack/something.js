cac976c2f23d7f2d7ee1df2173fa2720c1d34c77
Vladimir Yartsev <vovayartsev@gmail.com>
2016-05-31 02:36:20 +0400

    Moved compiler.js to gem

--- app/assets/webpack/something.js
+++ app/assets/webpack/something.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-console.log(8);
+console.log(2, React);
 
 //sdfasdfasdf{}-2!
 
--- gems/sprockets_require_webpack/lib/sprockets_require_webpack/webpack_driver.rb
+++ gems/sprockets_require_webpack/lib/sprockets_require_webpack/webpack_driver.rb
@@ -6,7 +6,7 @@ module SprocketsRequireWebpack
     def initialize(config, entry)
       @tempfile = Tempfile.new("bundle.XXXXXXXXX.js")
 
-      cmd = "node compiler.js #{config} #{entry} #{path.dirname} #{path.basename}"
+      cmd = "node #{compiler_js_path} #{config} #{entry} #{path.dirname} #{path.basename}"
       @io = IO.popen(cmd, 'r+')
     end
 
@@ -22,9 +22,18 @@ module SprocketsRequireWebpack
       end
 
       OpenStruct.new(errors: errors, data: path.read, benchmark: bm)
+    rescue Errno::EPIPE
+      OpenStruct.new(
+          errors: ['Broken pipe. Webpack died', 'Please check STDERR and restart web server'],
+          data: 'console.error("Broken pipe. Webpack died.")',
+          benchmark: "<#{@io.pid} died>")
     end
 
     private
+
+    def compiler_js_path
+      Pathname.new(__FILE__).dirname.join('compiler.js')
+    end
 
     def path
       @path ||= Pathname.new(@tempfile.path)
